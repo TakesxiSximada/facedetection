957f8eacb0a690fdec2e3ced3f176b26d2c79eb8
TakesxiSximada <takesxi.sximada@gmail.com>
2017-03-17 13:31:51 +0900

    add management commands.

---
+++ cmd.mk
@@ -0,0 +1,38 @@
+.DEFAULT_GOAL := help
+
+
+.PHONY: pypi
+pypi:
+	@# Upload test pypi
+
+	twine upload dist/*.whl -r pypi
+
+
+.PHONY: wheel
+wheel:
+	@# Build wheel package
+
+	rm -rf dist
+	python setup.py bdist_wheel --universal
+
+
+.PHONY: testpypi
+testpypi: wheel
+	@# Register to test pypi
+
+	twine register dist/*.whl -r testpypi
+	twine upload dist/*.whl -r testpypi
+
+
+.PHONY: clean
+clean:
+	@# clean build directory
+
+	rm -rf build dist
+
+
+.PHONY: help
+help:
+	@# Display usage
+
+	unmake $(MAKEFILE_LIST)
